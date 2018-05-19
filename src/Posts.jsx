2de05bb5e71c9d6ef8fb355a2421222653958e02
Pascal Hahn <hahnpascal@gmail.com>
2018-05-19 15:22:15 +0200

    added api to show some blog posts

---
+++ src/Posts.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import axios from 'axios';
+
+import Post from './Post';
+
+export default class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await axios('/posts');
+    this.setState({
+      posts: response.data,
+    });
+  }
+
+  render() {
+    const { posts } = this.state;
+    return posts.map(post => (
+      <Post key={post.id} title={post.title} body={post.body} />
+    ));
+  }
+}
